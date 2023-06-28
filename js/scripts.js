4fe4b77dffe60dcf184892b9e4874d3c385bb472
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-06-28 16:59:25 +0200

    ADD click first btn

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const posts = [
+const post = [
     {
         id : 1,
         author : {
@@ -39,39 +39,58 @@ const posts = [
 
 const container = document.getElementById('container');
 
-for (i = 0; i < posts.length; i++){
+for (i = 0; i < post.length; i++){
     container.innerHTML = container.innerHTML +
     `
     <div class="post">
             <div class="post__header">
                 <div class="post-meta">                    
                     <div class="post-meta__icon">
-                        <img class="profile-pic" src="${posts[i].author.photo}" alt="${posts[i].author.name}">                    
+                        <img class="profile-pic" src="${post[i].author.photo}" alt="${post[i].author.name}">                    
                     </div>
                     <div class="post-meta__data">
-                        <div class="post-meta__author">${posts[i].author.name}</div>
-                        <div class="post-meta__time">${posts[i].date}</div>
+                        <div class="post-meta__author">${post[i].author.name}</div>
+                        <div class="post-meta__time">${post[i].date}</div>
                     </div>                    
                 </div>
             </div>
-            <div class="post__text">${posts[i].text}</div>
+            <div class="post__text">${post[i].text}</div>
             <div class="post__image">
-                <img src="${posts[i].media}" alt="">
+                <img src="${post[i].media}" alt="">
             </div>
             <div class="post__footer">
                 <div class="likes js-likes">
                     <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <a class="like-button  js-like-button" href="#" data-postid="${post[i].id}">
                             <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                             <span class="like-button__label">Mi Piace</span>
                         </a>
                     </div>
                     <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${post[i].likes}</b> persone
                     </div>
                 </div> 
             </div>            
         </div>
     `
 }
+
+
+   
+let btnDiv = document.querySelector('.likes__cta');
+let btn = document.querySelector('.like-button');
+let likeCounter = document.querySelector('.js-likes-counter')
+
+btnDiv.addEventListener('click',
+    function(){
+        let likesClicked = post[0].likes;
+        console.log('PRIMA', likesClicked)
+        console.log('bottone cliccato');
+        likesClicked++;
+        console.log('DOPO', likesClicked);
+        btn.classList.add('like-button--liked');
+        likeCounter.innerHTML = likesClicked;
+        }
+)
+
 
