dd5742edd673eef5f8cbf7e48f8b79234fcad30a
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-06-28 15:37:17 +0200

    ADD array objects

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+const posts = [
+    {
+        id : 1,
+        author : {
+            name : 'Pierpaolo',
+            photo : 'https://unsplash.it/300/300?image='
+        },
+        date : '06/28/2023',
+        media : 'https://unsplash.it/300/300?image=',
+        likes : 100,
+        text : 'Lorem ipsum lorem ipsum' 
+    },
+
+    {
+        id : 2,
+        author : {
+            name : 'Pier',
+            photo : 'https://unsplash.it/300/300?image='
+        },
+        date : '07/28/2023',
+        media : 'https://unsplash.it/300/300?image=',
+        likes : 121,
+        text : 'Lorem ipsum lorem ipsum'
+    },
+    
+    {
+        id : 3,
+        author : {
+            name : 'Paolo',
+            photo : 'https://unsplash.it/300/300?image='
+        },
+        date : '05/28/2023',
+        media : 'https://unsplash.it/300/300?image=',
+        likes : 99,
+        text : 'Lorem ipsum lorem ipsum' 
+    }
+
+
+]
